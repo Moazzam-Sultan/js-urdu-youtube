e85b4917c99fbd4c769107ad6bc20965a2ea8b16
Moazzam-Sultan <mouzammughal187@gmail.com>
2024-08-04 14:42:32 +0500

    Add files via upload

---
+++ 01.variables.js
@@ -0,0 +1,7 @@
+const accountId = 23489;
+let accountEmail = "mouzammughal@gmail.com";
+var accountPasswaord = "1453";
+accountCity = "Lahore";
+
+console.log(accountId);
+console.table(accountEmail,accountId,accountPasswaord,);
